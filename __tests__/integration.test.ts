318715509132979e41b8aa5d4238127658359f01
savez.siddiqui <savez.siddiqui@zopsmart.com>
2021-06-21 16:43:13 +0530

    feat: adds tests for factorial route

--- __tests__/integration.test.ts
+++ __tests__/integration.test.ts
@@ -1,6 +1,7 @@
 import * as request from 'supertest'
 import { createWinstonLogger } from '../middleware/Logger'
 import server from '../server'
+import mockdate from 'mockdate'
 
 afterAll(() => {
   server.close()
@@ -33,7 +34,6 @@ describe('Tests for task1', () => {
 })
 
 describe('Tests for task2', () => {
-  ;``
   test('should return string after authentication', async () => {
     const response = await request(server)
       .get('/protected')
@@ -81,5 +81,87 @@ describe('tests for logger', () => {
       })
     await request(server).get('/hello')
     expect(spy).toHaveBeenCalledTimes(2)
+  })
+})
+
+describe('tests for factorial route', () => {
+  test(`should return factorial when query "request" is slow`, async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => 1487076708000)
+    const resp = await request(server)
+      .get('/api/v1/factorial/1000?request=slow')
+      .set('Authorization', 'Basic YWRtaW46MTIz')
+
+    expect(resp.body).toEqual({
+      data: {
+        factorial: {
+          value: 641419708,
+          timeTaken: 0,
+        },
+      },
+    })
+  })
+
+  test(`should return factorial when query "request" is fast`, async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => 1487076708000)
+    const resp = await request(server)
+      .get('/api/v1/factorial/10000?request=fast')
+      .set('Authorization', 'Basic YWRtaW46MTIz')
+
+    expect(resp.body).toEqual({
+      data: {
+        factorial: {
+          value: 531950724,
+          timeTaken: 0,
+        },
+      },
+    })
+  })
+
+  test('should return proper error message when number is out of range', async () => {
+    mockdate.set('2000-11-22')
+    const resp = await request(server)
+      .get('/api/v1/factorial/1000000000?request=fast')
+      .set('Authorization', 'Basic YWRtaW46MTIz')
+
+    expect(resp.body).toEqual({
+      error: {
+        reason: ' 1 <= number <= 1e8',
+        dateTime: '2000-11-22T00:00:00.000Z',
+        message: 'number out of range',
+        details: { number: 1e9 },
+      },
+    })
+  })
+
+  test('should return proper error message when query is invalid', async () => {
+    mockdate.set('2000-11-22')
+    const resp = await request(server)
+      .get('/api/v1/factorial/1000?request=GodKnowsWhat')
+      .set('Authorization', 'Basic YWRtaW46MTIz')
+
+    expect(resp.body).toEqual({
+      error: {
+        reason: 'request can either be fast or slow',
+        dateTime: '2000-11-22T00:00:00.000Z',
+        message: 'invalid request',
+        details: { request: 'GodKnowsWhat' },
+      },
+    })
+  })
+
+  test('should return proper error message when query is not given', async () => {
+    mockdate.set('2000-11-22')
+    const resp = await request(server)
+      .get('/api/v1/factorial/1000')
+      .set('Authorization', 'Basic YWRtaW46MTIz')
+
+    expect(resp.body).toEqual({
+      error: {
+        reason: 'request can either be fast or slow',
+        dateTime: '2000-11-22T00:00:00.000Z',
+        message: 'invalid request',
+        details: { request: '' },
+      },
+    })
   })
 })
